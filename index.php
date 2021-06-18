7196650019b52fe5c6db293b47ff91a5d41dd822
Robin Khiv <43227798+RobinKhiv@users.noreply.github.com>
2021-06-18 03:30:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,6 @@
   if(file_exists('./env.php'))
     include './env.php';
 
-  $variables = [
-      'USERNAME' => 'sample_api',
-      'PASSWORD' => 'GVBjPMm9s8qf8CZd',
-      'URL' => 'https://dev.driveitag.com/user/',
-  ];
-
-  foreach ($variables as $key => $value) {
-      putenv("$key=$value");
-  }
-
   $username = getenv('USERNAME');
   $password = getenv('PASSWORD');
   $url = getenv('URL');
